2d84b1ca5cc67d0e9199160311683897294b777c
Zhibin Huang <hzb.huang@mail.utoronto.ca>
2024-11-27 05:26:50 -0500

    Add files via upload

---
+++ scripts/03-clean_data.R
@@ -0,0 +1,35 @@
+#### Preamble ####
+# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
+# Author: Rohan Alexander [...UPDATE THIS...]
+# Date: 6 April 2023 [...UPDATE THIS...]
+# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# License: MIT
+# Pre-requisites: [...UPDATE THIS...]
+# Any other information needed? [...UPDATE THIS...]
+
+#### Workspace setup ####
+library(tidyverse)
+
+#### Clean data ####
+#load raw data
+data <- read_csv("data/01-raw_data/raw_data.csv", skip = 1)
+#clean column names
+colnames(data) <- c("Date",
+                    "Institutional",
+                    "Standard Error",
+                    "Individual",
+                    "Standard Error"
+)
+#select needed columns
+data <- data[,c(1,2,4)]
+#given Date format
+data$Date <- parse_date( paste0("01/",data$Date), format = "%d/%m/%Y")
+#convert numeric format
+data$Individual <- as.numeric(data$Individual)
+#missing values
+data <- data %>% drop_na()
+#only use data from 2020
+data <- data %>% filter(Date >= as.Date("2000-01-01"))
+
+#### Save data ####
+write.csv(data, file = "data/02-analysis_data/analysis_data.csv")
